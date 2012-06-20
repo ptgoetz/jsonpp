5e8cf955f11fd77a8809904b30a76c9b982fec53
P. Taylor Goetz <ptgoetz@gmail.com>
2012-06-20 19:35:11 -0400

    Initial Commit

---
+++ jsonpp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  jsonpp
+//
+//  Created by P. Taylor Goetz on 6/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
