e1b6ecc6458f95b847090c9fb004d5abdd0cf265
P. Taylor Goetz <ptgoetz@gmail.com>
2012-06-20 19:48:00 -0400

    initial commit

--- jsonpp/main.m
+++ jsonpp/main.m
@@ -3,20 +3,54 @@
 //  jsonpp
 //
 //  Created by P. Taylor Goetz on 6/20/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 InstanceOne, LLC. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        if(argc > 2){
+            printf("Usage: %s [file]\n", argv[0]);
+            return 1;
+        }
+        
+        NSLog(@"Argument count: %i", argc);
+        for (int i = 0; i < argc; i++) {
+            NSLog(@"argv[%i] %s", i, argv[i]);
+            NSString* arg = [[NSString alloc ] initWithCString:argv[i]encoding:NSUTF8StringEncoding];
+            NSLog(@"Current arg: %@", arg);
+        }
+        
+        NSData* dataIn = nil;
+        NSString* str = nil;
+        if(argc == 2){
+            NSString* file = [[NSString alloc ] initWithCString:argv[1]encoding:NSUTF8StringEncoding];
+            NSString* str = [[NSString alloc] initWithContentsOfFile:file encoding:NSUTF8StringEncoding error:nil];
+            dataIn = [str dataUsingEncoding:NSUTF8StringEncoding];
+        } else {
+            dataIn = [[NSFileHandle fileHandleWithStandardInput] readDataToEndOfFile];
+        }
+        
+        NSError *theError = nil;
+        NSData* jsonDataIn = [NSJSONSerialization JSONObjectWithData:dataIn options:NSJSONReadingAllowFragments error:&theError];
+        
+        if(theError == nil){
+            NSData* jsonDataOut = [NSJSONSerialization dataWithJSONObject:jsonDataIn 
+                                                                  options:NSJSONWritingPrettyPrinted error:&theError];
+            str = [[NSString alloc] initWithData:jsonDataOut encoding:NSUTF8StringEncoding];
+            const char* c = [str UTF8String];
+            
+            printf("%s\n", c);
+            return 0;
+        } else{
+            printf("Parse Error: %s\n", [[[theError userInfo] valueForKey:@"NSDebugDescription"] UTF8String]);
+            return 1;
+        }
         
     }
-    return 0;
 }
 
